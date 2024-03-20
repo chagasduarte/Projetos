414432b93a89403e6469f82af59bfb0500c3c3b9
chagasduarte <70854311+chagasduarte@users.noreply.github.com>
2024-03-20 09:37:57 -0300

    Adicionado método get à controller de livros

---
+++ biblioteca-webapi/biblioteca-webapi/Controllers/LivrosController.cs
@@ -0,0 +1,28 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+
+namespace biblioteca_webapi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LivrosController : ControllerBase
+    {
+        private readonly string _connectionString;
+        public LivrosController(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("biblioteca");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            using (var sqlConnection = new SqlConnection(_connectionString))
+            {
+                const string sql = "SELECT * FROM livro";
+                var livros = await sqlConnection.QueryAsync(sql);
+                return Ok(livros);
+            }
+        }
+    }
+}
