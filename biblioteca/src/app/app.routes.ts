9f2d114bbd4ddcdb61559bfb5f0691fef58edd21
chagasduarte <70854311+chagasduarte@users.noreply.github.com>
2024-03-19 22:00:31 -0300

    Adicionado pasta do projeto da webApi

--- biblioteca/src/app/app.routes.ts
+++ biblioteca/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { LivrosComponent } from './pages/livros/livros.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component: LivrosComponent}
+];
