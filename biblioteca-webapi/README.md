88464c8e1b1debc829a5e13d18d176bdff7bd0c3
chagasduarte <70854311+chagasduarte@users.noreply.github.com>
2024-03-21 08:03:12 -0300

    Removido projeto WebApi para atualizar gitignore

---
+++ biblioteca-webapi/README.md
@@ -0,0 +1,8 @@
+# Biblioteca - WebApi  
+<div style="display: inline_block">
+  <img align="center" alt="c#" src="https://img.shields.io/badge/C%23-239120?style=for-the-badge&logo=c-sharp&logoColor=white"/>
+  <img align="center" alt=".net" src="https://img.shields.io/badge/.NET-5C2D91?style=for-the-badge&logo=.net&logoColor=white"/>
+  <img align="center" alt="sqlserver" src="https://img.shields.io/badge/Microsoft_SQL_Server-CC2927?style=for-the-badge&logo=microsoft-sql-server&logoColor=white"/>
+</div><br/>
+Este projeto foi desenvolvido em C# com .net 6.0, utilizando o banco SQL Server da Microsoft, com o objetivo de treinar minhas habilides com a linguagem.
+
---
+++ biblioteca-webapi/biblioteca-webapi/Controllers/LivrosController.cs
@@ -0,0 +1,49 @@
+﻿using biblioteca_webapi.Entities;
+using biblioteca_webapi.Models;
+using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+
+namespace biblioteca_webapi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LivrosController : ControllerBase
+    {
+        private readonly string _connectionString;
+        public LivrosController(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("biblioteca");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            using (var sqlConnection = new SqlConnection(_connectionString))
+            {
+                const string sql = "SELECT * FROM livro";
+                var livros = await sqlConnection.QueryAsync<Livros>(sql);
+                return Ok(livros);
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Insert(LivroInputModel livro)
+        {
+            var parameters = new
+            {
+                livro.Nome,
+                livro.Usuario,
+                livro.Box,
+
+            };
+            using (var sqlConnection = new SqlConnection(_connectionString))
+            {
+                const string sql = "INSERT INTO [dbo].[livro] OUTPUT INSERTED.id_livro  VALUES (@nome,@usuario ,@box)";
+                int id = await sqlConnection.ExecuteScalarAsync<int>(sql, parameters);
+                return Ok(id);
+            }
+        }
+
+    }
+}
